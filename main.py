02e5838b07228c02b0119ec999cf0de7cfeab970
DeepeshKalura <deepeshkalurs@gmail.com>
2024-03-29 00:30:16 +0530

    Add CORS middleware and serve HTML template file

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-def root():
-    return { 
-            "message": "welecome to our project auto"
-        }
+async def root():
+    return FileResponse("template/home.html")
+
